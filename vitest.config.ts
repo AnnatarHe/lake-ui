a98b2a8e7fc940ef52455caa3aec1f289261dd4a
AnnatarHe <annatar.he+github@gmail.com>
2025-02-12 22:46:32 +0800

    feat(app): setup ci and tests

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+ 
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    coverage: {
+      include: ['src/**/*.{ts,tsx}'],
+    },
+    globals: true,
+    setupFiles: ['./tests/setup.ts'],
+    environment: 'happy-dom',
+  },
+})

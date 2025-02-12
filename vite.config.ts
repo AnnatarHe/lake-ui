9999e0dc4736358d8082fb1877f78b0971c23b17
AnnatarHe <annatar.he+github@gmail.com>
2025-02-12 09:35:12 +0800

    fix(ci): add basic build workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'LakeUI',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    },
+  },
 })

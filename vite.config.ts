8048f4645b46a93d656076068bae655df6eab90f
AnnatarHe <annatar.he+github@gmail.com>
2025-02-13 00:23:46 +0800

    fix(build): add missing dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      tsconfigPath: 'tsconfig.app.json',
+    })
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',

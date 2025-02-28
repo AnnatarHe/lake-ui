30baa13ad785855d69e1594b7a61a0da59f8b550
AnnatarHe <annatar.he+github@gmail.com>
2025-02-28 16:12:28 +0800

    fix(lint): fix lint issue

--- src/contribution-wall/index.tsx
+++ src/contribution-wall/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react'
+import { useMemo } from 'react'
 import { addDays, fromUnixTime, startOfDay, toUnixTime } from '../utils/date'
 
 import { percentilesOf } from '../utils/percentiles'

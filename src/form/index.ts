12f3d5c8a613504d942945dcb6f6b347163cd90a
AnnatarHe <annatar.he+github@gmail.com>
2025-04-26 21:28:45 +0800

    fix(textarea): add entry

--- src/form/index.ts
+++ src/form/index.ts
@@ -3,5 +3,13 @@ import MultiSelect from './multi-select'
 import NumberField from './number-field'
 import SelectField from './select-field'
 import SwitchField from './switch-field'
+import TextareaField from './textarea-field'
 
-export { InputField, MultiSelect, NumberField, SelectField, SwitchField }
+export {
+  InputField,
+  MultiSelect,
+  NumberField,
+  SelectField,
+  SwitchField,
+  TextareaField,
+}
